e4b2e395d52945baa7b4374fae0e0b2a5ab4eb55
Kane Lincoln <Kane.Lincoln@icloud.com>
2018-08-16 18:01:43 +0100

    Added env and dbURI variables to config/environment and added CORS headers to index

--- config/environment.js
+++ config/environment.js
@@ -1,3 +1,5 @@
-const port = process.env.PORT || 4000;
+const PORT = process.env.PORT || 4000;
+const env = process.env.NODE_ENV || 'dev';
+const dbURI = process.env.DB_URI || `mongodb://localhost/traverse-${env}`;
 
-module.exports = { port };
+module.exports = { PORT, dbURI };
