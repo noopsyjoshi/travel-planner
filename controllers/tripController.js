948aec11f4c6f83621759bbc1254631b2423a830
NoopurJoshiGA <noopur.joshi13@gmail.com>
2018-08-17 14:26:17 +0100

    added trip controller functions

---
+++ controllers/tripController.js
@@ -0,0 +1,43 @@
+const Trip = require('../models/trip');
+
+// trips show is the end result page, where the user can see the itenarary of their trip
+function tripsShow(req, res, next) {
+  Trip
+    .findById(req.params.id)
+    .then(trip => res.json(trip))
+    .catch(next);
+}
+
+// user can create a new trip
+function tripsCreate(req, res, next) {
+  Trip
+    .create(req.body)
+    .then(trip => res.status(201).json(trip))
+    .catch(next);
+}
+
+// user can update their trip from their dashboard
+function tripsUpdate(req, res, next) {
+  Trip
+    .findById(req.params.id)
+    .then(trip => trip.set(req.body))
+    .then(trip => trip.save())
+    .then(trip => res.json(trip))
+    .catch(next);
+}
+
+// user can delete their recent trip from their dashboard
+function tripsDelete(req, res, next) {
+  Trip
+    .findById(req.params.id)
+    .then(trip => trip.remove())
+    .then(() => res.sendStatus(204)) // NO CONTENT
+    .catch(next);
+}
+
+module.exports = {
+  show: tripsShow,
+  create: tripsCreate,
+  update: tripsUpdate,
+  delete: tripsDelete
+};
