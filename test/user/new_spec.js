11d5a8480e93cf07319cef0e57c5258988869e1d
Kane Lincoln <Kane.Lincoln@icloud.com>
2018-08-18 16:51:23 +0100

    Added a test for creating new users

---
+++ test/user/new_spec.js
@@ -0,0 +1,32 @@
+/* global describe, it, expect, api, beforeEach */
+
+const User = require('../../models/user');
+const jwt = require('jsonwebtoken');
+const { secret } = require('../../config/environment');
+let token;
+
+const userData = {
+  firstName: 'test',
+  lastName: 'user',
+  username: 'testuser',
+  email: 'test@test.com',
+  password: 'pass'
+};
+
+
+describe('POST /register', () => {
+  beforeEach(done => {
+    User.remove({});
+    done();
+  });
+
+  // Creating a new user
+  it('should return a 200', done => {
+    api.post('/api/register')
+      .send(userData)
+      .end((err, res) => {
+        expect(res.status).to.eq(200);
+        done();
+      });
+  });
+});
