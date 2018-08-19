bb898399ba30dbc9b139a84787f04a151b1f1dde
NoopurJoshiGA <noopur.joshi13@gmail.com>
2018-08-19 18:03:30 +0100

    added interest, list of all activities to form

--- src/controllers/trips/new.js
+++ src/controllers/trips/new.js
@@ -1,4 +1,4 @@
-function TripsNewCtrl($scope) {
+function TripsNewCtrl($scope, $http) {
   console.log('into the new trip controller...');
 
   // Trip Duration Variables
@@ -16,6 +16,14 @@ function TripsNewCtrl($scope) {
 
   // This will display the type of accomodation user has selected
   const selectedAccomodationType = document.querySelector('#accomodationType');
+
+  // Interests Variables
+  const buttonMusic = document.querySelector('#btnMusic');
+  const buttonHistory = document.querySelector('#btnHistory');
+  const buttonRestaurants = document.querySelector('#btnRestaurants');
+
+  const selectedInterests = document.querySelector('#interests');
+
 
   // Get Trip Duration
   $scope.getDurationThreeDays = function() {
@@ -48,6 +56,45 @@ function TripsNewCtrl($scope) {
     console.log('user has selected', buttonBedBreakfast.innerHTML );
     selectedAccomodationType.innerHTML = buttonBedBreakfast.innerHTML;
   };
+
+  // Get
+  $scope.getInterestMusic = function() {
+    console.log('user has selected', buttonMusic.innerHTML);
+    // selectedInterests.innerHTML = buttonMusic.innerHTML;
+    const music = document.createElement('li');
+    music.classList.add('box');
+    music.innerHTML = buttonMusic.innerHTML;
+    selectedInterests.appendChild(music);
+  };
+
+  $scope.getInterestHistory = function() {
+    console.log('user has selected', buttonHistory.innerHTML);
+    // selectedInterests.innerHTML = buttonHistory.innerHTML;
+    const history = document.createElement('li');
+    history.classList.add('box');
+    history.innerHTML = buttonHistory.innerHTML;
+    selectedInterests.appendChild(history);
+  };
+
+  $scope.getInterestRestaurants = function() {
+    console.log('user has selected', buttonRestaurants.innerHTML);
+    // selectedInterests.innerHTML = buttonRestaurants.innerHTML;
+    const restaurants = document.createElement('li');
+    restaurants.classList.add('box');
+    restaurants.innerHTML = buttonRestaurants.innerHTML;
+    selectedInterests.appendChild(restaurants);
+  };
+
+  // Make a request to the database to get the activities based on interests
+  $http({
+    method: 'GET',
+    url: '/api/activities'
+  })
+  // The activities information is in the data section in console
+    .then(res => {
+      console.log('Activities are', res.data);
+      $scope.activities = res.data;
+    });
 }
 
 
