c329fac36028fa33aa612bfbc1619b69a35b8759
NoopurJoshiGA <noopur.joshi13@gmail.com>
2018-08-20 17:56:33 +0100

    refactored duration in new.js and new.html

--- src/controllers/trips/new.js
+++ src/controllers/trips/new.js
@@ -1,104 +1,24 @@
-function TripsNewCtrl($scope, $http) {
+function TripsNewCtrl($scope, $http, $rootScope) {
   console.log('into the new trip controller...');
 
   // Trip Duration Variables
-  const buttonThree = document.querySelector('#btnThree');
-  const buttonSeven = document.querySelector('#btnSeven');
-  const buttonTen = document.querySelector('#btnTen');
+  $scope.durations = [3, 7, 10];
 
   // This is the span that will display the user's choice
-  const selectedDuration = document.querySelector('#tripDuration');
-
-  // Accomodation type Variables
-  const buttonHotel = document.querySelector('#btnHotel');
-  const buttonHostel = document.querySelector('#btnHostel');
-  const buttonBedBreakfast = document.querySelector('#btnBedBreakast');
+  const selectedDurationSpan = document.querySelector('#tripDuration');
 
   // This will display the type of accomodation user has selected
   const selectedAccomodationType = document.querySelector('#accomodationType');
 
-  // Interests Variables
-  const buttonMusic = document.querySelector('#btnMusic');
-  const buttonHistory = document.querySelector('#btnHistory');
-  const buttonRestaurants = document.querySelector('#btnRestaurants');
-
   // This will display the interests in the html
   const selectedInterests = document.querySelector('#interests');
 
-  // Get Trip Duration
-  $scope.getDurationThreeDays = function() {
-    console.log('user has selected', buttonThree.innerHTML );
-    selectedDuration.innerHTML = buttonThree.innerHTML;
-    $scope.selectedDuration = 3;
-    // $scope.selectedDuration should return 3
-    console.log('$scope with the duration is ', $scope.selectedDuration);
-  };
-
-  $scope.getDurationSevenDays = function() {
-    console.log('user has selected', buttonSeven.innerHTML );
-    selectedDuration.innerHTML = buttonSeven.innerHTML;
-    $scope.selectedDuration = 7;
-    // $scope.selectedDuration should return 7
-    console.log('$scope with the duration is ', $scope.selectedDuration);
-  };
-
-  $scope.getDurationTenDays = function() {
-    console.log('user has selected', buttonTen.innerHTML );
-    selectedDuration.innerHTML = buttonTen.innerHTML;
-    $scope.selectedDuration = 10;
-    // $scope.selectedDuration should return 10
-    console.log('$scope with the duration is ', $scope.selectedDuration);
-  };
-
-  // Get Accomodation Type
-  $scope.getTypeHotel = function() {
-    console.log('user has selected', buttonHotel.innerHTML );
-    selectedAccomodationType.innerHTML = buttonHotel.innerHTML;
-    $scope.selectedAccomodationType = 'Hotel';
-    console.log('$scope with the selected accomodation type is ', $scope.selectedAccomodationType);
-  };
-
-  $scope.getTypeHostel = function() {
-    console.log('user has selected', buttonHostel.innerHTML );
-    selectedAccomodationType.innerHTML = buttonHostel.innerHTML;
-    $scope.selectedAccomodationType = 'Hostel';
-    console.log('$scope with the selected accomodation type is ', $scope.selectedAccomodationType);
-  };
-
-  $scope.getTypeBedBreakfast = function() {
-    console.log('user has selected', buttonBedBreakfast.innerHTML );
-    selectedAccomodationType.innerHTML = buttonBedBreakfast.innerHTML;
-    $scope.selectedAccomodationType = 'Bed & Breakfast';
-    console.log('$scope with the selected accomodation type is ', $scope.selectedAccomodationType);
-  };
-
-  // Get
-  $scope.getInterestMusic = function() {
-    console.log('user has selected', buttonMusic.innerHTML);
-    // selectedInterests.innerHTML = buttonMusic.innerHTML;
-    const music = document.createElement('li');
-    music.classList.add('box');
-    music.innerHTML = buttonMusic.innerHTML;
-    selectedInterests.appendChild(music);
-    $scope.music = 'music';
-  };
-
-  $scope.getInterestHistory = function() {
-    console.log('user has selected', buttonHistory.innerHTML);
-    // selectedInterests.innerHTML = buttonHistory.innerHTML;
-    const history = document.createElement('li');
-    history.classList.add('box');
-    history.innerHTML = buttonHistory.innerHTML;
-    selectedInterests.appendChild(history);
-  };
-
-  $scope.getInterestRestaurants = function() {
-    console.log('user has selected', buttonRestaurants.innerHTML);
-    // selectedInterests.innerHTML = buttonRestaurants.innerHTML;
-    const restaurants = document.createElement('li');
-    restaurants.classList.add('box');
-    restaurants.innerHTML = buttonRestaurants.innerHTML;
-    selectedInterests.appendChild(restaurants);
+  // Refactored!!!!!!
+  $scope.getDuration = function($event) {
+    console.log($event.target.getAttribute('durationVal'));
+    const selectedDuration = $event.target.getAttribute('durationVal');
+    // $rootScope.trip should reflect your model
+    $rootScope.trip.duration = selectedDuration;
   };
 
   // Make a request to the database to get the activities based on interests
