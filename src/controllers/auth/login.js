b4a8a1720b94a053051a55d93544616a20304bff
NoopurJoshiGA <noopur.joshi13@gmail.com>
2018-08-21 18:22:14 +0100

    fixed dashboard (usersShow) link

--- src/controllers/auth/login.js
+++ src/controllers/auth/login.js
@@ -1,11 +1,29 @@
 
-function AuthLoginCtrl($scope, $http, $auth, $state) {
+function AuthLoginCtrl($scope, $http, $auth, $state, $rootScope) {
+
+  $scope.user = {
+    email: 'noopurjoshi@gmail.com',
+    password: 'pass'
+  };
+
   $scope.login = function() {
     console.log('Log in controller');
     $auth.login($scope.user)
       .then(() => {
-        console.log('Logging in user...', $scope.user);
+        console.log('There is now a userId stored on the token!', $auth.getPayload().sub);
+        // Make the logged in user's ID available everywhere!
+        $rootScope.userId = $auth.getPayload().sub;
         $state.go('tripsNew1');
+        $http({
+          method: 'GET',
+          url: `/api/users/${$rootScope.userId}`
+        })
+        // retrieve the id in the response
+          .then(res => {
+            console.log('Found user with id ', res.data);
+            // put user id on root scope (available everywhere!)
+            $rootScope.user = res.data;
+          });
       })
       .catch(err => console.log('There was an error', err));
   };
--- src/controllers/users/show.js
+++ src/controllers/users/show.js
@@ -1,16 +1,6 @@
-function UsersShowCtrl($http, $state, $scope) {
+function UsersShowCtrl($http, $state, $scope, $rootScope) {
   console.log('This is the users show controller...');
   // make a request to db to get the user id
-  $http({
-    method: 'GET',
-    url: `/api/user/${$state.params.id}`
-  })
-  // retrieve the id in the response
-    .then(res => {
-      console.log('Found user with id ', res.data);
-      // put user id on scope
-      $scope.user = res.data;
-    });
 }
 
 export default UsersShowCtrl;
