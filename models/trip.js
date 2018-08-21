59a418825a2580f1084708488bd5623a6e2f527a
Laukso <l.m.auksoriute@gmail.com>
2018-08-21 11:21:00 +0100

    added accommodation to trips bundle

--- models/trip.js
+++ models/trip.js
@@ -5,7 +5,12 @@ const tripSchema = new mongoose.Schema({
   city: { type: String, required: true },
   duration: { type: String, required: false  },
   accomodationType: [{ type: String, required: false  }],
-  accommodation: [{ type: String, required: false  }],
+  accommodation: [
+    {
+      accommodation: { type: mongoose.Schema.ObjectId, ref: 'Accommodation'},
+      date: { type: Date }
+    }
+  ],
   budget: {type: Number, required: false  },
   categories: [{ type: String, required: false }],
   activities: [
