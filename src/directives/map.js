e00d30e91a6d747256d184bbdb88e00b23caf2d4
Laukso <l.m.auksoriute@gmail.com>
2018-08-22 09:32:53 +0100

    added ng-map directives to 3 views

--- src/directives/map.js
+++ src/directives/map.js
@@ -1,6 +1,6 @@
 /* global L */
 
-function Map($http) {
+function Map() {
   return { // Return the object from the database
 
     restrict: 'A', // Chooses directive type // A - sets functionality to DOM element
@@ -9,59 +9,42 @@ function Map($http) {
       const domElement = $element[0]; // Creates a DOM element
       const map = L.map(domElement);
 
+      // MAP SETTINGS
       L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png?access_token={accessToken}', {
         attribution: 'Map data © <a href="https://openstreetmap.org">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-        maxZoom: 18,
-        id: 'mapbox.satellite',
+        maxZoom: 18, // sets zoom level
+        id: 'mapbox.satellite', // sets map view type
         accessToken: 'your.mapbox.access.token'
-      }).addTo(map);
+      }).addTo(map); // map is ready for use
 
-      const doge = L.icon({
-        iconUrl: 'http://www.stickpng.com/assets/thumbs/5845e69dfb0b0755fa99d7ef.png',
+      // MARKER PARAMETERS
+      const greenIcon = L.icon({
         iconSize: [100, 100], // size of the icon
         iconAnchor: [22, 94], // point of the icon which will correspond to marker's location
         popupAnchor: [-3, -76] // point from which the popup should open relative to the iconAnchor
       });
 
-      // Our map is ready to use
-      // (Note: it still needs position and zoom level)
-      map.on('click', event => {
-        console.log(event.latlng);
-        L.marker([event.latlng.lat, event.latlng.lng], {icon: doge}).addTo(map);
+      // DISPLAY MARKERS ON MAP ON CLICK
+      map.on('click', e => {
+        L.marker([e.latlng.lat, e.latlng.lng], {icon: greenIcon}).addTo(map);
       });
 
-      // get our location and add a marker
-      // if will ask the user if they want to allow user to share location if true it will run if not it wont
-      if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(position => {
-          const marker = L.marker([position.coords.latitude, position.coords.longitude], {icon: doge}).addTo(map);
-          marker.bindPopup('<p>Im here</p>');
-        });
-      }
-
-      // need to wait for whiskey to load first. saying when this watch finishes of whikey then run the function
-      $scope.$watch('whiskey', function() {
-        if($scope.whiskey) {
-          $http({
-            method: 'GET',
-            url: `https://nominatim.openstreetmap.org/search/${$scope.whiskey.originCountry}?format=json`
-          })
-          // then we get a response hopefully. res.data is all the data
-            .then(res => {
-              const place = res.data.sort((a, b) => a.importance < b.importance)[0]; // <-- this is javascript sorting
-              // place is now available! set it on the $scope
-              $scope.place = place;
-              // map.setView([place.lat, place.lon], 5); // number 5 means zoom level
-              const bb = place.boundingbox; // getting this from insomnia
-              map.fitBounds([
-                [ bb[0], bb[2] ], // top left // brackets extracts element from the arrya
-                [ bb[1], bb[3] ] // bottom right
-              ]);
-              const marker = L.marker([place.lat, place.lon]).addTo(map);
-              marker.bindPopup(`<p>${place.display_name}</p>`)
-            });
+      // MAP DISPLAY FUNCTION
+      $scope.$watch('activity', function() { // Watching for the page to load first.
+        if($scope.activity) {
+          const lat = $scope.activity.coordinates.latitude; // getting data from seeds and storing in a new variable
+          const lng = $scope.activity.coordinates.longitude; // getting data from seeds and storing in a new variable
+          map.setView([lat, lng], 12); //
+          const marker = L.marker([lat, lng]).addTo(map);
+          marker.bindPopup('<p>test</p>');
         }
       });
+
+      function onMapClick(e) {
+        console.log('You clicked the map at ' + e.latlng);
+      }
+      map.on('click', onMapClick);
+
     }
   };
 }
